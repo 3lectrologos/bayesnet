dbca29e3910cfbc07b88564369524afd285f8f6b
Alkis Gotovos <alkisg@inf.ethz.ch>
2014-10-21 15:55:11 +0200

    Fix demo to correspond to final form of hw3

--- demo.py
+++ demo.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import examples_dsep
 
 
-g = examples_dsep.bn_5_earthquake()
-g.get_reachable('Earthquake', plot=True)
+g = examples_dsep.bn_earthquake()
+g.get_reachable('Radio', plot=True)
 plt.show()
-g.get_reachable('Earthquake', ['Alarm'], plot=True)
+g.get_reachable('Radio', ['Phone'], plot=True)
+plt.show()
+g.get_reachable('Radio', ['Phone', 'Earthquake'], plot=True)
 plt.show()
