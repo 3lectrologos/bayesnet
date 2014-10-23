12ca2a8471dff91d61e9c9363fd154212e7f82a1
Josip Djolonga <josipd@inf.ethz.ch>
2014-10-23 23:07:00 +0200

    Fixes the test to use the new refactored code

--- test/test_bprop.py
+++ test/test_bprop.py
@@ -1,17 +1,19 @@
 import unittest2
-from .. import bprop
-from ..examples_bprop import *
+from ..examples_bprop import bn_earthquake
+from ..bprop import FactorGraph
 
 
 class TestBeliefPropagation(unittest2.TestCase):
     def test_earthquake_1(self):
-        g = bprop.earthquake()
-        g.condition({'Phone': 1})
-        marg, _, _ = g.run_bp(10)
+        g = bn_earthquake()
+        fg = FactorGraph(g)
+        fg.condition({'Phone': 1})
+        marg, _, _ = fg.run_bp(10)
         self.assertAlmostEqual(marg['Burglar'][-1, 0], 0.505, places=3)
 
     def test_earthquake_2(self):
-        g = bprop.earthquake()
-        g.condition({'Phone': 1, 'Radio': 1})
-        marg, _, _ = g.run_bp(10)
+        g = bn_earthquake()
+        fg = FactorGraph(g)
+        fg.condition({'Phone': 1, 'Radio': 1})
+        marg, _, _ = fg.run_bp(10)
         self.assertAlmostEqual(marg['Burglar'][-1, 0], 0.917, places=3)
