108e2641bdc4359623cd5e644f4c387b637cca6a
Alkis Gotovos <alkisg@inf.ethz.ch>
2014-11-11 15:14:53 +0100

    Some fixes to sampling; add documentation

--- demo.py
+++ demo.py
@@ -4,23 +4,20 @@ import examples_bprop
 import sampling
 
 
-# Load coin flipping Bayes network and plot it.
+# Load coin flipping Bayes network and create Gibbs sampler.
 bn = examples_bprop.bn_naive_bayes()
 g = bprop.FactorGraph(bn)
-
-# Create a Gibbs sampler and draw 20,000 samples, after a burn-in period
-# of 1,000 samples.
 sampler = sampling.GibbsSampler(g)
-marg = sampler.run(20000, burnin=1000)
 
-# Draw the cumulative average marginals after each sample.
+# Draw 10,000 samples, after a 100-sample burn-in and plot approximate
+# cumulative marginals.
+marg = sampler.run(10000, burnin=100)
 bprop.draw_marginals(marg, markers=False)
 plt.show()
 
 # Do the same, when conditioned on having observed heads, tails, heads.
-g.condition({'X1': 'H', 'X2': 'T', 'X3': 'H'})
-sampler = sampling.GibbsSampler(g)
-marg = sampler.run(20000, burnin=1000)
+sampler.condition({'X1': 'H', 'X2': 'T', 'X3': 'H'})
+marg = sampler.run(10000, burnin=100)
 bprop.draw_marginals(marg, markers=False)
 plt.show()
 
@@ -28,20 +25,17 @@ plt.show()
 bn = examples_bprop.bn_earthquake()
 g = bprop.FactorGraph(bn)
 sampler = sampling.GibbsSampler(g)
-marg = sampler.run(20000, burnin=1000)
+
+marg = sampler.run(20000, burnin=100)
 bprop.draw_marginals(marg, markers=False)
 plt.show()
 
-g = bprop.FactorGraph(bn)
-g.condition({'Phone': 1})
-sampler = sampling.GibbsSampler(g)
-marg = sampler.run(20000, burnin=1000)
+sampler.condition({'Phone': 1})
+marg = sampler.run(20000, burnin=100)
 bprop.draw_marginals(marg, markers=False)
 plt.show()
 
-g = bprop.FactorGraph(bn)
-g.condition({'Phone': 1, 'Radio': 1})
-sampler = sampling.GibbsSampler(g)
-marg = sampler.run(20000, burnin=1000)
+sampler.condition({'Radio': 1})
+marg = sampler.run(20000, burnin=100)
 bprop.draw_marginals(marg, markers=False)
 plt.show()
--- sampling.py
+++ sampling.py
@@ -1,25 +1,86 @@
-import math
 import numpy as np
 import numpy.random as npr
-import matplotlib.pylab as plt
 import bprop
-import examples_bprop
+
+
+def cumavg(array, step=1):
+    """Compute cumulative average of ``array``.
+
+    The result is an array, such that, for all 0 <= i <= k*step
+
+    cumavg[i] = (array[0] + array[step] + ... + array[k*step]) / (k + 1),
+
+    where k = floor(len(array) / step).
+
+    Arguments
+    ---------
+    array : iterable
+        Input array.
+
+    step : int
+        Step size (only every step-th value of ``array`` is considered).
+
+    Returns
+    -------
+    A (k + 1) x 1 array of cumulative averages.
+    """
+    array = np.array(array)
+    avg = np.cumsum(array[::step, :], dtype=float)
+    avg /= np.arange(1, len(avg) + 1)
+    avg = np.reshape(avg, (-1, 1))
+    return avg
+
+
+def avg(array, step=1):
+    """Compute the average of ``array`` taking into account only every
+    ``step``-th value.
+    """
+    array = np.array(array)
+    return np.reshape(np.mean(array[::step]), (-1, 1))
 
 
 class GibbsSampler:
     def __init__(self, fgraph):
         self.fgraph = fgraph
-        self.vs = fgraph.vs
+        self.update_fgraph()
 
-    # NOTE: These could be precomputed if we want to be more efficient.
+    def update_fgraph(self):
+        """Should be called when the associated factor graph is updated."""
+        self.vs = self.fgraph.vs
+        self.vobs = self.fgraph.vobs
+
+    def condition(self, observations):
+        """Convenience method. Same as ``bprob.FactorGraph.condition``."""
+        self.fgraph.condition(observations)
+        self.update_fgraph()
+
+    # NOTE: We could precompute/memoize the posteriors for more efficiency.
     def sample_var(self, v, state):
+        """Sample a value of variable ``v`` from its posterior given ``state``.
+
+        We need to only consider the values of variables in ``state`` that
+        belong to the Markov blanket of ``v``, equivalently, all variables that
+        participate in factors that are neighbors of ``v`` in the factor graph.
+
+        Arguments
+        ---------
+        v : str
+            Name of the variable to be updated.
+
+        state : dict
+            Current state of the Gibbs sampler.
+
+        Returns
+        -------
+        A randomly sampled value of ``v`` from the posterior P(v | state\{v}).
+        """
         v_domain = self.vs[v].domain
         prob = np.zeros(len(v_domain))
         for d in v_domain:
             for fnode in self.vs[v].neighbors:
-                # Create one by one the variable combination corresponding to
-                # the current state for all other variables and a value of d
-                # for the variable to be sampled.
+                # Add variables one by one to create the combination that has
+                # the same values as the current state for all other variables
+                # in the factor and a value of d for variable v.
                 comb = []
                 for fnode_var in fnode.variables:
                     if fnode_var == v:
@@ -30,15 +91,44 @@ class GibbsSampler:
         prob = bprop.normalize(prob)
         return npr.choice(v_domain, p=np.exp(prob))
 
-    def run(self, niter, burnin=0, init_state=None):
+    def run(self, niter, burnin=0, init_state=None, fcum=cumavg):
+        """Run Gibbs sampler to obtain ``niter`` samples.
+
+        Optionally, use a burn-in period during which samples are discarded,
+        and specify (part of) the starting state. Furthermore, a function
+        ``fcum`` for approximating marginals can be specified (see
+        ``get_marginals`` for more details).
+
+        Arguments
+        ---------
+        niter : int
+            Number of samples to be returned.
+
+        burnin : int
+            Length of burn-in period.
+
+        init_state : dict
+            Starting state. Can be specified partially by only providing
+            initial values for a subset of all variables.
+
+        fcum : fun
+            Function for computing marginals (see ``get_marginals``).
+
+        Returns
+        -------
+        A tuple of computed marginals, variable domains, and observations,
+        same as that returned by ``bprob.FactorGraph.run_bp``.
+        """
         assert burnin < niter
         variables = self.vs.keys()
         samples = {v: [] for v in variables}
+        # If not specified, the initial value of each variable is drawn
+        # uniformly at random.
         state = {v: npr.choice(vnode.domain)
                  for v, vnode in self.vs.items()}
         if init_state is not None:
             state.update(init_state)
-        # Burn-in period: samples are discarded.
+        # Burn-in period: samples are drawn and discarded.
         for it in range(burnin):
             current_variable = npr.choice(variables)
             state[current_variable] = self.sample_var(current_variable, state)
@@ -48,23 +138,43 @@ class GibbsSampler:
             state[current_variable] = self.sample_var(current_variable, state)
             for v in variables:
                 samples[v].append(state[v])
-        marg = self.get_marginals(samples)
+        marg = self.get_marginals(samples, fcum)
         domains = {v.name: v.orig_domain for v in self.vs.values()}
         return (marg, domains, self.fgraph.vobs)
 
-    def get_marginals(self, samples):
+    def get_marginals(self, samples, fcum):
+        """Compute approximate marginals by applying ``fcum`` to ``samples``.
+
+        For every value a variable can take, a binary indicator array is
+        created that indicates at which iterations the variable had that value.
+        Then, the given function ``fcum`` is applied to each of those binary
+        arrays. ``fcum`` should return a r x 1 array (r >= 1).
+
+        Arguments
+        ---------
+        samples : dict
+            Dictionary that maps each variable to its samples as produced by
+            the Gibbs sampler.
+
+        fcum : fun
+            A function that computes a r x 1 array per variable.
+
+        Returns
+        -------
+        A dictionary that maps each variable v to a r x |domain(v)| array,
+        i.e., each column contains the results of ``fcum`` for each value of v.
+        """
         niter = len(samples.values()[0])
-        marg = {v: np.zeros((niter, 0)) for v in samples}
-        # For each variable v, marg[v] is a niter x |domain(v)| matrix.
+        assert niter >= 1
+        marg = {v: None for v in samples}
         for v in samples:
-            samples_v = np.array(samples[v])
+            v_samples = np.array(samples[v])
             for d in self.vs[v].domain:
-                samples_d = np.zeros((niter, 1))
-                samples_d[samples_v == d] = 1
-                # Compute cumulative average, that is, for all 0 <= i < niter:
-                #     cumavg[i] = (value_0 + ... + value_i) / (i + 1)
-                cumavg = np.cumsum(samples_d, dtype=float)
-                cumavg /= np.arange(1, len(cumavg) + 1)
-                cumavg = np.reshape(cumavg, (-1, 1))
-                marg[v] = np.hstack((marg[v], cumavg))
+                bin_array = np.zeros((niter, 1))
+                bin_array[v_samples == d] = 1
+                cum = fcum(bin_array)
+                if marg[v] is None:
+                    marg[v] = cum
+                else:
+                    marg[v] = np.hstack((marg[v], cum))
         return marg
