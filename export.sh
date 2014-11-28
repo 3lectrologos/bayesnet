6eeb0ce1ad50e08f5aa0ad4fbfd78401dde72ef1
Alkis Gotovos <alkisg@inf.ethz.ch>
2014-11-28 20:31:33 +0100

    Export hw5 solution

--- export.sh
+++ export.sh
@@ -3,3 +3,4 @@ git archive -o hw3_sol.zip --prefix hw3_sol/ dsep_solution
 git archive -o hw4.zip --prefix hw4/ bprop
 git archive -o hw4_sol.zip --prefix hw4_sol/ bprop_solution
 git archive -o hw5.zip --prefix hw5/ sampling
+git archive -o hw5_sol.zip --prefix hw5_sol/ sampling_solution
